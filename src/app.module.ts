1f2d725d2c888ed46fdfd8f8cd7c57bac33b1739
Paula Misas <pmisas@unal.edu.co>
2023-07-05 12:23:39 -0500

    :100:

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,26 @@ import { ProyectsModule } from './modules/proyects/proyects.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 
+const bd_options = {
+  "host": process.env.DB_HOST,
+  "port": parseInt(process.env.DB_PORT || '5432'),
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database":  process.env.DB_NAME,
+  "entities": [__dirname + '**/entities/*.entity{.ts,.js}'],
+  "synchronize": true,
+}
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       "type": 'mysql',
-      "host": 'b7b5ypanwlmolmkztjsm-mysql.services.clever-cloud.com',
-      "port": 3306,
-      "username": 'ue00drfpqhozx7go',
-      "password": 'FwZ4ADfiXnc5vsJJlpPq',
-      "database": 'b7b5ypanwlmolmkztjsm',
+      //"host": 'b7b5ypanwlmolmkztjsm-mysql.services.clever-cloud.com',
+      //"port": 3306,
+      //"username": 'ue00drfpqhozx7go',
+      //"password": 'FwZ4ADfiXnc5vsJJlpPq',
+      //"database": 'b7b5ypanwlmolmkztjsm',
+      ...bd_options,
       "entities": [__dirname + '**/entities/*.entity{.ts,.js}'],
       "synchronize": true,
     }),
