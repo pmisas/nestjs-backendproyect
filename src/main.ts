6e314a4f1c73d999018ee5a2506f4d8153e7b64e
Paula Misas <pmisas@unal.edu.co>
2023-07-06 08:22:20 -0500

    :100:

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,10 @@ async function bootstrap() {
     origin: 'https://angularcatask.web.app',
     credentials: true
   })
-  const host = process.env.HOST || '0.0.0.0';
-  const port = process.env.PORT || 3000;
+  //const host = process.env.HOST || '0.0.0.0';
+  //const port = process.env.PORT || 3000;
 
-  await app.listen(port, host);
+  await app.listen(8080, '0.0.0.0');
 
 }
 bootstrap();
