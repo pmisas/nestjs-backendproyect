ef9ee23359ab7723b95207cb76a95806caade365
Paula Misas <pmisas@unal.edu.co>
2023-07-06 07:17:11 -0500

    :100:

--- dist/main.js
+++ dist/main.js
@@ -10,7 +10,7 @@ async function bootstrap() {
         origin: 'https://angularcatask.web.app',
         credentials: true
     });
-    await app.listen(process.env.PORT);
+    await app.listen('https://console.clever-cloud.com/users/me/applications/app_45fd2a13-64eb-4ce3-803f-9ea4e101e0ef/logs');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
