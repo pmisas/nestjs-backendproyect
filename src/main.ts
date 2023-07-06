5a8c7478a179f364e8979f1e00df8b84085693d2
Paula Misas <pmisas@unal.edu.co>
2023-07-06 08:53:42 -0500

    :+1:

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   //const host = process.env.HOST ;
   //const port = process.env.PORT ;
 
-  await app.listen(8080, '0.0.0.0')
+  await app.listen(process.env.PORT || 3000)
 
 }
 bootstrap();
